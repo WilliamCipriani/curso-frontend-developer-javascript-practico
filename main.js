5b8ad426060a58aa21bb8eea3b4ed1c00f096229
administrador23 <87953444+WilliamCipriani@users.noreply.github.com>
2023-07-23 17:11:01 -0500

    union del carrito de compras

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleProductDetail);
 
 //La función permite alternar la visibilidad del menú en la página al agregar o quitar la clase 'inactive'
 function toggleDesktopMenu() { 
@@ -12,5 +15,22 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() { 
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive'); 
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleProductDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive'); 
+    }
+
+    aside.classList.toggle('inactive');
+
 }
