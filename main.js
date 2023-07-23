f33bad12f378037ab053f38afa1b05806206633f
administrador23 <87953444+WilliamCipriani@users.noreply.github.com>
2023-07-23 16:39:26 -0500

    navbar-menu-mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-// La función permite alternar la visibilidad del menú en la página al agregar o quitar la clase 'inactive'
+//La función permite alternar la visibilidad del menú en la página al agregar o quitar la clase 'inactive'
 function toggleDesktopMenu() { 
- desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() { 
+    mobileMenu.classList.toggle('inactive')
 }
